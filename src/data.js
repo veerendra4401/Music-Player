9580d3660db24cc8af25df30c836eef2e5bf58ae
veerendra4401 <thogitiveerendra5@gmail.com>
2025-01-22 19:53:43 +0530

    Music-Player

---
+++ src/data.js
@@ -0,0 +1,73 @@
+import { v4 as uuidv4 } from "uuid";
+function chillHop() {
+  return [
+    {
+      name: "Sunrise Serenade",
+      cover:
+"https://static.toiimg.com/thumb/msid-103359834,width-1070,height-580,imgsize-82196,resizemode-75,overlay-toi_sw,pt-32,y_pad-40/photo.jpg",
+      artist: " Harmony Harp",
+      audio:
+"https://media.geeksforgeeks.org/wp-content/uploads/20231004185212/Jawan-Prevue-Theme.mp3",
+      color: ["#205950", "#2ab3bf"],
+      id: uuidv4(),
+      active: true,
+    },
+    {
+      name: "Urban Groove",
+      cover:
+        "https://i.pinimg.com/736x/0f/d8/57/0fd8577fe8c1c38f28280f9cc3e8d237.jpg",
+      artist: "Beatmaster B",
+      audio:
+        "https://media.geeksforgeeks.org/wp-content/uploads/20231004184006/SoundHelix-Song-10.mp3",
+      color: ["#EF8EA9", "#ab417f"],
+      id: uuidv4(),
+      active: false,
+    },
+    {
+      name: "Mystic Echo",
+      cover:
+"https://static.vecteezy.com/system/resources/previews/008/653/792/non_2x/music-man-gamer-line-pop-art-potrait-logo-colorful-design-with-dark-background-abstract-illustration-isolated-black-background-for-t-shirt-poster-clothing-merch-apparel-badge-design-vector.jpg",
+      artist: " Harmony Harp",
+      audio:
+"https://media.geeksforgeeks.org/wp-content/uploads/20231004185212/Jawan-Prevue-Theme.mp3",
+      color: ["#CD607D", "#c94043"],
+      id: uuidv4(),
+      active: false,
+    },
+    {
+      name: "Electro Vibes",
+      cover:
+"https://t4.ftcdn.net/jpg/05/78/50/75/360_F_578507584_y0BUZ9yYbIZy9lNVI4sH1AbuLJaBPJru.jpg",
+      artist: "Synthwave Sensation",
+      audio:
+"https://media.geeksforgeeks.org/wp-content/uploads/20231004191840/Zinda-Banda---Jawan-(1).mp3",
+      color: ["#EF8EA9", "#ab417f"],
+      id: uuidv4(),
+      active: false,
+    },
+    {
+      name: "Jazzy Whispers",
+      cover:
+"https://cdn.dribbble.com/userupload/16883018/file/still-3774e1e5d84d11ac8f54a28a759f02ca.gif?resize=400x0.jpg",
+      artist: "Smooth Sax Serenade",
+      audio:
+"https://media.geeksforgeeks.org/wp-content/uploads/20231004184006/SoundHelix-Song-10.mp3",
+      color: ["#CD607D", "#c94043"],
+      id: uuidv4(),
+      active: false,
+    },
+    {
+      name: "Tropical Breez",
+      cover:
+"https://easy-peasy.ai/cdn-cgi/image/quality=80,format=auto,width=700/https://fdczvxmwwjwpwbeeqcth.supabase.co/storage/v1/object/public/images/b3142387-afea-4691-a21c-f7c8440fd544/cdcce133-b5c4-4a85-abe8-066138b3c98d.png",
+      artist: "Island Rhythms",
+      audio:
+"https://media.geeksforgeeks.org/wp-content/uploads/20231004191840/Zinda-Banda---Jawan-(1).mp3",
+      color: ["#205950", "#2ab3bf"],
+      id: uuidv4(),
+      active: false,
+    },
+  ];
+}
+
+export default chillHop;
